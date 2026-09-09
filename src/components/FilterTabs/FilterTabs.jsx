e9dcff8c7d495f1df01b3abd66c7bfb76c312da2
Eman Ahmed <emo216276@gmail.com>
2026-09-09 23:59:24 +0300

    Create project structure and header component

---
+++ src/components/FilterTabs/FilterTabs.jsx
@@ -0,0 +1,7 @@
+export default function FilterTabs(){
+    return(
+        <>
+        <h1>FilterTabs</h1>
+        </>
+    )
+}
---
+++ src/components/TaskItem/TaskItem.jsx
@@ -0,0 +1,7 @@
+export default function TaskItem(){
+    return(
+        <>
+        <h1>TaskItem</h1>
+        </>
+    )
+}
